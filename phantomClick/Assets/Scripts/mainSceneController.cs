efcd50cdf9535498446bebb7798f4fbd27c13e50
orion <orion@play-i.com>
2015-11-24 15:18:23 -0800

    files

---
+++ phantomClick/Assets/Scripts/mainSceneController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class mainSceneController : MonoBehaviour {
+
+	public Button btn;
+	public Text   txt;
+	
+	private       float btnClickedTime = 0;
+	private const float countdownSecs  = 4;
+	
+
+	// Use this for initialization
+	void Start () {
+		btn.onClick.AddListener(onClickBtn);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (btnClickedTime == 0) {
+			return;
+		}
+		
+		float t = countdownSecs - (Time.time - btnClickedTime);
+		if (t > 0) {
+			txt.text = "press-and-hold here!\n" + t.ToString("0.0") + "..";
+		}
+		else {
+			gameObject.SetActive(false);
+			Application.LoadLevel("scene2");
+		}
+	}
+	
+	void onClickBtn() {
+		btnClickedTime = Time.time;
+	}
+}
---
+++ phantomClick/Assets/Scripts/scene2Controller.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class scene2Controller : MonoBehaviour {
+
+	public Button btn;
+	public Text   txt;
+	public Button doItAgain;
+
+	// Use this for initialization
+	void Start () {
+		btn.onClick.AddListener(onClickBtn);
+		
+		doItAgain.onClick.AddListener(onClickDoItAgain);
+		doItAgain.gameObject.SetActive(false);
+	}
+	
+	void onClickBtn() {
+		txt.text = "You Clicked It!";
+		doItAgain.gameObject.SetActive(true);
+	}
+	
+	void onClickDoItAgain() {
+		Application.LoadLevel("main");
+	}
+}
